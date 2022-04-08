e2748f74e9a62b4c1611cf959617bcc47a6c7578
katherine-kania <katherine.kania@gmail.com>
2022-04-08 08:01:56 -0400

    search components merged

--- src/components/Search/GetSearch.js
+++ src/components/Search/GetSearch.js
@@ -1,22 +1,20 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap';
 import { getAllSearch } from '../../api/search.js'
-import apiUrl from '../../apiConfig.js';
 import { useNavigate } from 'react-router-dom';
-import {getSearchSuccess, getSearchFailure} from '../shared/AutoDismissAlert/messages'
 import SearchIndex from '../Search/SearchIndex'
 
 // we need to render a form that allows the user to search (events, venues, performers)
 const GetSearch = (props) => {
-    const {user, msgAlert} = props
+    const { user, msgAlert } = props
     const navigate = useNavigate()
-// we want to set state defined as search with type and name as ...
-  
- const [search, setSearch] = useState({type: null, name: null})
- 
- const [searchResults, setSearchResults] = useState(null)
+    // we want to set state defined as search with type and name as ...
+
+    const [search, setSearch] = useState({ type: null, name: null })
+
+    const [searchResults, setSearchResults] = useState(null)
     // we want to pass in the values we get from search forms
-    
+
     // that we assigned to state and pass them in as parameters 
     // to getAllSearch
     // then have something that handles the change (handleChange function)
@@ -36,77 +34,96 @@ const GetSearch = (props) => {
             // }else if(type === "performers" && name === " ") {
             //     value = parseInt(e.target.value)
             // }
+            // if (search.type == 'performers') {
+            //    let [type] = 'performers'  
+            // } else if (search.type == 'events'){
+            //     let [type] = 'events'
+            // } else if (search.type == 'venues'){
+            //     let [type] = 'venues'
+            // } 
             const updatedValue = { [name]: value }
 
 
-            console.log('this is prevSearch' , prevSearch)
+            console.log('this is prevSearch', prevSearch)
             console.log('updatedValue', updatedValue)
 
-            return {...prevSearch, ...updatedValue}
-    })
+            return { ...prevSearch, ...updatedValue }
+        })
 
-}
+    }
 
     const handleSubmit = (e) => {
         // e === event
-      
+
 
         e.preventDefault()
+        let searchType = search.type
         getAllSearch(search.type, search.name)
-        // if create is successful we shoudl navigate to the show page
-        .then(res => {
-            setSearchResults(res.data.performers)
-            console.log(searchResults)    
-            //navigate(`/search/${search.type}/${search.name}`
-            
-       // )
-        })
+        
+            // if create is successful we shoudl navigate to the show page
+            .then(res => {
+                if (search.type === 'performers'){
+                    setSearchResults(res.data.performers)
+                } else if (search.type === 'events'){
+                    setSearchResults(res.data.events)
+                } else if (search.type === 'venues'){
+                    setSearchResults(res.data.venues)
+                } else {
+                    return null
+                }
+                console.log('this is the res.data', res.data)
+
+                console.log('this is searchResults', searchResults)
+                //navigate(`/search/${search.type}/${search.name}`
+
+                // )
+            })
         // console.log(res.data.search)
-       //console.log(search.type)
+        //console.log(search.type)
         // {navigate(`${apiUrl}/search/${search.type}/${search.name}`)}
-           //then we send a success message
-    //        .then(() =>
-    //        msgAlert({
-    //            heading: 'Yay!',
-    //            message: getSearchSuccess,
-    //            variant: 'success',
-    //        }))
-    //    // if there is an error, we'll send an error message
-    //    .catch(() =>
-    //        msgAlert({
-    //            heading: 'Oh No!',
-    //            message: getSearchFailure,
-    //            variant: 'danger',
-    //        }))
+        //then we send a success message
+        //        .then(() =>
+        //        msgAlert({
+        //            heading: 'Yay!',
+        //            message: getSearchSuccess,
+        //            variant: 'success',
+        //        }))
+        //    // if there is an error, we'll send an error message
+        //    .catch(() =>
+        //        msgAlert({
+        //            heading: 'Oh No!',
+        //            message: getSearchFailure,
+        //            variant: 'danger',
+        //        }))
 
-       // console.log('this is the type', type)
+        // console.log('this is the type', type)
     }
-
+    console.log('this is searchResults 2', searchResults)
     // want something to submit the form (getAllSearch)
-    return(
+    return (
         <>
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId='type'>
-                <Form.Control
-                    name="type"
-                    placeholder='events, performers, venues'
-                    value={search.type}
-                    onChange={handleChange}
-                />
+            <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId='type'>
+                    <Form.Select name="type" onChange={handleChange} aria-label="Default select example">
+                        <option>Select Your Type</option>
+                        <option value={search.type}>events</option>
+                        <option value={search.type}>performers</option>
+                        <option value={search.type}>venues</option>
+                    </Form.Select>
                 </Form.Group>
                 <Form.Group controlId='name'>
-                <Form.Control
-                    name="name"
-                    placeholder='name of event, artist, or venue'
-                    value={search.name}
-                    onChange={handleChange}
-                />
+                    <Form.Control
+                        name="name"
+                        placeholder='name of event, artist, or venue'
+                        value={search.name}
+                        onChange={handleChange}
+                    />
                 </Form.Group>
-            <Button variant="primary" type="submit" >
-                SEARCH
-            </Button>
-        </Form>
-        <SearchIndex searchResults={searchResults} search={search}/>
+                <Button variant="primary" type="submit" >
+                    SEARCH
+                </Button>
+            </Form>
+        <SearchIndex searchResults={searchResults} search={search} />
         </>
     )
 }
--- src/components/Search/SearchShow.js
+++ src/components/Search/SearchShow.js
@@ -1,23 +1,59 @@
 import React, { useState } from 'react'
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button, Container } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import apiUrl from '../../apiConfig.js';
 import { getOneSearch } from '../../api/search.js'
 import { useEffect } from 'react';
-import { showSearchFailure, showSearchSuccess } from '../shared/AutoDismissAlert/messages'
+
+const cardContainerLayout = {
+    display: 'flex',
+    justifyContent: 'center',
+    flexFlow: 'row wrap',
+    flexDirection: 'column',
+    AlignItem: 'center'
+}
 
 const SearchShow = (props) => {
     const { type, name, id } = useParams()
-    const { user, msgAlert, searchResults } = props
-    const [showResult, setShowResult] = useState({})
+    const { user, msgAlert, searchResults, search } = props
 
+    const [showResult, setShowResult] = useState({})
+    console.log('this is search', search)
+    console.log('this is user', user)
+    console.log('this is searchResults', searchResults)
     useEffect(() => {
         getOneSearch(type, name, id)
+            // .then(res => {
+            //     setShowResult(res.data.performers[0])
+            //     console.log('this is the res.data.performers[0].name', showResult)
+            //     console.log('this is the res.data.performers[0].name', res.data.performers)
+            // })
+
             .then(res => {
-                setShowResult(res.data.performers[0])
-                console.log('this is the res.data.performers[0].name', showResult)
-                console.log('this is the res.data.performers[0].name', res.data.performers[0].name)
+                // if(res.showResult === 'performers'){}
+                if (type === 'performers') {
+                    setShowResult(res.data.performers[0])
+                } else if (type === 'events') {
+                    setShowResult(res.data.events[0])
+                } else if (type === 'venues') {
+                    setShowResult(res.data.venues[0])
+                } else {
+                    return null
+                }
+
+                // else if (search.type === 'events'){
+                //     setShowResult(res.data.events[0])
+                // } else if (search.type === 'venues'){
+                //     setShowResult(res.data.venues[0])
+                // } else {
+                // return null
+                // }
+                console.log('this is the res.data', res.data)
+
+                // console.log('this is showResults', showResult)
+                //navigate(`/search/${search.type}/${search.name}`
+
+                // )
             })
         // .catch(() => {
         //     msgAlert({
@@ -27,27 +63,128 @@ const SearchShow = (props) => {
         //     })
         // })
     }, [])
-    return (
-        <>
-            <Card style={{ width: '30%' }} className="m-2">
+    console.log('this is the showResult 2', showResult)
 
-                <Card.Header>
+    if (showResult.type === 'band') {
+
+        return (
+            <>
+                <Card style={{ width: '30%' }} className="m-2">
+                    <Card.Header>
+                        {showResult.name}
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            <Card.Img variant="bottom" src={showResult.image} />
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Link style={{ textDecoration: "none" }}
+                    to={`/search`}>
+                    <Button>
+                        Back to search
+                    </Button>
+                </Link>
+            </>
+        )
+    } else if (showResult.type === 'music_festival') {
+        return (
+            <>
+                <Card style={{ width: '30%' }} className="m-2">
                     {showResult.name}
-                </Card.Header>
-                <Card.Body>
-                    <Card.Text>
-                        <Card.Img variant="bottom" src={showResult.image} />
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-            <Link style={{ textDecoration: "none" }}
-                to={`/search`}>
-                <Button>
-                    Back to search
-                </Button>
-            </Link>
-        </>
-    )
+                    <Card.Header>
+                        {showResult.short_title}
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            {showResult.datetime_local}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Link style={{ textDecoration: "none" }}
+                    to={`/search`}>
+                    <Button>
+                        Back to search
+                    </Button>
+                </Link>
+            </>
+        )
+    } else if (showResult.country === 'US') {
+        return (
+            <>
+                <Card style={{ width: '30%' }} className="m-2">
+                    <Card.Header>
+                        {showResult.name}
+                        <small>
+                            {showResult.city}, {showResult.state}
+                        </small>
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            {showResult.url}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Link style={{ textDecoration: "none" }}
+                    to={`/search`}>
+                    <Button>
+                        Back to search
+                    </Button>
+                </Link>
+            </>
+
+        )
+    } else {
+
+        return (
+            <>
+                <Container style={cardContainerLayout} className="fluid">
+
+                    <Card style={{ width: '30%' }} className="m-2">
+
+                        <Card.Header>
+                            {showResult.name}
+                        </Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                <Card.Img variant="bottom" src={showResult.image} />
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Link style={{ textDecoration: "none" }}
+                        to={`/search`}>
+                        <Button>
+                            Back to search
+                        </Button>
+                    </Link>
+                </Container>
+            </>
+        )
+
+
+
+        // one method of styling, usually reserved for a single style
+        // we can use inline, just like in html
+
+
+
+        // one method of styling, usually reserved for a single style
+        // we can use inline, just like in html
+
+
+
+        // one method of styling, usually reserved for a single style
+        // we can use inline, just like in html
+
+
+
+
+
+
+        {/* <Form hidden>
+            </Form> */}
+
+    }
 }
 
 export default SearchShow
